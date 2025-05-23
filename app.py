540562b5aab571bc247a6aa0262ee65940fa241a
Ntk Nguyen <ntknguyen@Ntks-MacBook-Air.local>
2025-05-23 12:07:21 +0700

    🛠 Fix Telegram webhook route

--- app.py
+++ app.py
@@ -263,4 +263,5 @@ def telegram_webhook():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
